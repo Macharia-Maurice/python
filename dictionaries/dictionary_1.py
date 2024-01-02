fd8a4437b855a5ab7b01c875b6ca68a5cdc5e5a6
maurice <maurice.maina@students.jkuat.ac.ke>
2024-01-02 23:01:08 +0300

    updates

--- dictionaries/dictionary_1.py
+++ dictionaries/dictionary_1.py
@@ -6,7 +6,13 @@ students={
     'Peter':'Medicine'
 }
 
+# print out the whole dictionary including the curly braces
+print(students)
+
+# print out only the keys
+for student in students:
+    print(student)
+
 # printing out keys with their values
 for student in students:
-    
     print(student,'>>',students[student],)
---
+++ dictionaries/dictionary_2.py
@@ -0,0 +1,8 @@
+students={
+    'Hermione':'Gryffindor',
+    'Harry':'Gryffindor',
+    'Ron':'Slytherin'
+}
+
+for student in students:
+    print(students[student])
