78dfcef8223bf99e2944a6a1f9f21ce60fe101e1
maurice <maurice.maina@students.jkuat.ac.ke>
2024-01-02 22:45:36 +0300

    file

---
+++ dictionaries/dictionary_1.py
@@ -0,0 +1,12 @@
+students={
+    'Maurice':"Cs",
+    'Daniel':'financial Eng',
+    'Cynthia':'Actuarial Sc',
+    'Ann':'Medicine PSY',
+    'Peter':'Medicine'
+}
+
+# printing out keys with their values
+for student in students:
+    
+    print(student,'>>',students[student],)
