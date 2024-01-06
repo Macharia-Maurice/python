6bb48ee0798b24a138e513b00bf040b12dfd9347
maurice <maurice.maina@students.jkuat.ac.ke>
2024-01-07 00:44:25 +0300

    updates

--- Sqlite/app.py
+++ Sqlite/app.py
@@ -1,2 +1,12 @@
 import database
-database.show_all()
+# database.delete_record('12')
+# database.add_one_record('june','may','may@gmail')
+# items=[
+#     ('june','may','may@gmail'),
+#     ('solly', 'march', 'march@gmail')
+# ]
+# database.add_many_records(items)
+# database.show_all()
+
+person_x=database.search_by_id('10')
+print(person_x)
--- Sqlite/database.py
+++ Sqlite/database.py
@@ -1,20 +1,29 @@
 import sqlite3
 
+conn=sqlite3.connect('customer.db')
+c=conn.cursor()
 
 def show_all():
-    conn=sqlite3.connect('customer.db')
-    c=conn.cursor()
-
     c.execute("select rowid,* from customers ")
-    persons=c.fetchall()
 
+    persons=c.fetchall()
     for person in persons:
         print(person)
 
     conn.close()
 
-show_all()
+def add_one_record(first_name,last_name,email):
+    c.execute("insert into customers values(?,?,?)",(first_name,last_name,email))
+    conn.commit()
 
+def delete_record(rowid):
+    c.execute("delete from customers where rowid=(?)",(rowid,)) #pass parameters as a tuple
+    conn.commit()
 
-# c.execute("select rowid,* from customers ")
-# print(c.fetchall())
+def add_many_records(list):
+    c.executemany("insert into customers values(?,?,?)",(list))
+    conn.commit()
+
+def search_by_id(id):
+    c.execute("select * from customers where rowid=(?)", (id,))
+    return c.fetchall()
